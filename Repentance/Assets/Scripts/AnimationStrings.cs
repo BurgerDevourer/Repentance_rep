4207d3adb4ace69b74135f3fedc263ac67fad7b3
BurgerDevourer <elliott.kinell@icloud.com>
2025-04-11 19:01:13 +0200

    from dad
    map and movement

---
+++ Repentance/Assets/Scripts/AnimationStrings.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public static class AnimationStrings
+{
+    // Movement-related animation parameters
+    public const string IsMoving = "isMoving";
+    public const string IsRunning = "isRunning";
+}
--- Repentance/Assets/Scripts/PlayerController.cs
+++ Repentance/Assets/Scripts/PlayerController.cs
@@ -1,19 +1,81 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    public float walkSpeed = 5f;
+    public float walkSpeed = 1f;
+    public float runSpeed = 2.5f;
+
+    public float CurrentMoveSpeed { get 
+    {
+        if (IsMoving)
+        {
+            if(IsRunning)
+            {
+                return runSpeed;
+            } else 
+            {
+                return walkSpeed;
+            }
+        } else 
+        {
+            //idle speed
+            return 0;
+        }
+    }
+        
+    }
     Vector2 moveInput;
 
-    public bool IsMoving { get; private set; }
+    [SerializeField]
+    private bool _isMoving = false;
+
+    public bool IsMoving { get
+        {
+            return _isMoving;
+        } 
+        private set
+        {
+            _isMoving = value;
+            animator.SetBool(AnimationStrings.IsMoving, value);
+        }
+    }
+    [SerializeField]
+    private bool _isRunning = false;
+
+    public bool IsRunning { get 
+        {
+            return _isRunning;
+        }
+        set {
+            _isRunning = value;
+            animator.SetBool(AnimationStrings.IsRunning, value);
+        }
+
+    }
+
+    public bool _isFacingRight = true;
+    public bool IsFacingRight { get 
+        {
+            return _isFacingRight;
+        }
+        private set {
+            if (_isFacingRight != value) {
+                transform.localScale *= new Vector2(-1, 1);
+            }
+            _isFacingRight = value;
+        }
+    }
 
     Rigidbody2D rb;
+    Animator animator;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Start()
@@ -29,12 +91,38 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(moveInput.x * walkSpeed, rb.linearVelocity.y);
-        }
+        rb.linearVelocity = new Vector2(moveInput.x * CurrentMoveSpeed, rb.linearVelocity.y);
+    }
 
     public void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
         IsMoving = moveInput != Vector2.zero;
+
+        SetFacingDirection(moveInput);
+    }
+
+    private void SetFacingDirection(Vector2 moveInput)
+    {
+        if (moveInput.x > 0 && !IsFacingRight) 
+        {
+            IsFacingRight = true;
+        }
+        else if (moveInput.x < 0 && IsFacingRight)
+        {
+            IsFacingRight = false;
+        }
+    }
+
+    public void OnRun(InputAction.CallbackContext context) 
+    {
+        if (context.started)
+        {
+            IsRunning = true;
+        }
+        else if (context.canceled)
+        {
+            IsRunning = false;
+        }
     }
 }
